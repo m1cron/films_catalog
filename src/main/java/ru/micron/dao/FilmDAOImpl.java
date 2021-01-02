c1752ba6a4f9d8c82cc2488d8831c17176707639
m1cron <m1cron4900@gmail.com>
2021-01-02 21:40:04 +0300

    rewrited CRUD methods for Hibernate

--- src/main/java/ru/micron/dao/FilmDAOImpl.java
+++ src/main/java/ru/micron/dao/FilmDAOImpl.java
@@ -1,58 +1,48 @@
 package ru.micron.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.micron.model.Film;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FilmDAOImpl implements FilmDAO {
-    private static final AtomicInteger AUTO_ID = new AtomicInteger(0);
-    private static final Map<Integer, Film> films = new HashMap<>();
+    private SessionFactory sessionFactory;
 
-    static {
-        Film film1 = new Film();
-        film1.setId(AUTO_ID.getAndIncrement());
-        film1.setTitle("Inception");
-        film1.setYear(2010);
-        film1.setGenre("sci-fi");
-        film1.setWatched(true);
-        films.put(film1.getId(), film1);
-
-        Film film2 = new Film();
-        film2.setId(AUTO_ID.getAndIncrement());
-        film2.setTitle("TEST");
-        film2.setYear(1488);
-        film2.setGenre("test");
-        film2.setWatched(true);
-        films.put(film2.getId(), film2);
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Film> allFilms() {
-        return new ArrayList<>(films.values());
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Film").getResultList();
     }
 
     @Override
     public void add(Film film) {
-        film.setId(AUTO_ID.getAndIncrement());
-        films.put(film.getId(), film);
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(film);
     }
 
     @Override
     public void delete(Film film) {
-        films.remove(film.getId());
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(film);
     }
 
     @Override
     public void edit(Film film) {
-        films.put(film.getId(), film);
+        Session session = sessionFactory.getCurrentSession();
+        session.update(film);
     }
 
     @Override
     public Film getById(int id) {
-        return films.get(id);
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Film.class, id);
     }
 }
--- src/main/java/ru/micron/service/FilmServiceImpl.java
+++ src/main/java/ru/micron/service/FilmServiceImpl.java
@@ -4,32 +4,38 @@ import ru.micron.dao.FilmDAO;
 import ru.micron.dao.FilmDAOImpl;
 import ru.micron.model.Film;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public class FilmServiceImpl implements FilmService {
     private final FilmDAO filmDAO = new FilmDAOImpl();
 
     @Override
+    @Transactional
     public List<Film> allFilms() {
         return filmDAO.allFilms();
     }
 
     @Override
+    @Transactional
     public void add(Film film) {
         filmDAO.add(film);
     }
 
     @Override
+    @Transactional
     public void delete(Film film) {
         filmDAO.delete(film);
     }
 
     @Override
+    @Transactional
     public void edit(Film film) {
         filmDAO.delete(film);
     }
 
     @Override
+    @Transactional
     public Film getById(int id) {
         return filmDAO.getById(id);
     }
