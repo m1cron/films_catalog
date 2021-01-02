7590a7b593199f322085ba653c712f4aafe2f927
m1cron <m1cron4900@gmail.com>
2021-01-02 19:59:45 +0300

    added Film model

---
+++ src/main/java/ru/micron/model/Film.java
@@ -0,0 +1,49 @@
+package ru.micron.model;
+
+public class Film {
+    private int id;
+    private String title;
+    private int year;
+    private String genre;
+    private boolean watched;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+}
