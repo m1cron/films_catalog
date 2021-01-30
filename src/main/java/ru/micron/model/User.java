ba2d37eec1d04755771591492ebe17b35b145a07
m1cron <m1cron4900@gmail.com>
2021-01-30 22:53:16 +0300

    added user entity

---
+++ src/main/java/ru/micron/model/User.java
@@ -0,0 +1,30 @@
+package ru.micron.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+}
