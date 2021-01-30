de7d00159cc721f5ea7eaa9967bfe01ed7a051dd
m1cron <m1cron4900@gmail.com>
2021-01-30 21:18:47 +0300

    simplified code with lombok

--- src/main/java/ru/micron/model/Film.java
+++ src/main/java/ru/micron/model/Film.java
@@ -1,9 +1,12 @@
 package ru.micron.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "films")
+@Data
 public class Film {
 
     @Id
@@ -22,46 +25,6 @@ public class Film {
 
     @Column(name = "watched")
     private boolean watched;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public boolean isWatched() {
-        return watched;
-    }
-
-    public void setWatched(boolean watched) {
-        this.watched = watched;
-    }
 
     @Override
     public String toString() {
