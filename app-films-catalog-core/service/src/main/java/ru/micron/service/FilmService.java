5ae2a5218b7b8009c4ed969592b73d51d2b3789a
m1cron <m1cron4900@gmail.com>
2021-05-16 19:20:43 +0300

    deleted useless classes

--- app-films-catalog-core/service/src/main/java/ru/micron/service/FilmService.java
+++ app-films-catalog-core/service/src/main/java/ru/micron/service/FilmService.java
@@ -1,16 +1,31 @@
 package ru.micron.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.micron.model.Film;
+import ru.micron.repository.FilmRepository;
 
 import java.util.List;
 
-public interface FilmService {
+@Service
+@RequiredArgsConstructor
+public class FilmService {
 
-  Film findById(Long id);
+  private final FilmRepository filmRepository;
 
-  List<Film> findAll();
+  public Film findById(Long id) {
+    return filmRepository.findById(id).orElse(null);
+  }
 
-  void save(Film object);
+  public List<Film> findAll() {
+    return filmRepository.findAll();
+  }
 
-  void deleteById(Long id);
+  public void save(Film film) {
+    filmRepository.save(film);
+  }
+
+  public void deleteById(Long id) {
+    filmRepository.deleteById(id);
+  }
 }
