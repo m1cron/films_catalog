664f91faa92f257f52e55cafd561bc37e5104acd
m1cron <m1cron4900@gmail.com>
2022-05-14 20:33:36 +0300

    fixed RoleRepository

--- app-core/src/main/java/ru/micron/persistence/repository/RoleRepository.java
+++ app-core/src/main/java/ru/micron/persistence/repository/RoleRepository.java
@@ -1,9 +1,10 @@
 package ru.micron.persistence.repository;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.micron.persistence.model.RoleEntity;
 
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
 
   RoleEntity findByName(String name);
 }
